1b4006c23cf9482ce316ba21699385272c28bc57
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-07-21 13:39:43 +0300

    created javascript for massage about

--- massage/massage_Assets/massage_javascript/massage.js
+++ massage/massage_Assets/massage_javascript/massage.js
@@ -1,2 +1,62 @@
-let kenya =document.getElementById('doja');
-console.log(kenya)
+let aroma = document.getElementById("Aromatherapy");
+function getElement1() {
+  if (aroma.style.display === "none") {
+    aroma.style.display = "block";
+  } else {
+    aroma.style.display = "none";
+  }
+}
+
+let cran = document.getElementById("CranioSacral");
+function getElement2() {
+  if (cran.style.display === "none") {
+    cran.style.display = "block";
+  } else {
+    cran.style.display = "none";
+  }
+}
+
+let deep1 = document.getElementById("Deep");
+function getElement3() {
+  if (deep1.style.display === "none") {
+    deep1.style.display = "block";
+  } else {
+    deep1.style.display = "none";
+  }
+}
+
+let reiki1 = document.getElementById("reiki");
+function getElement4() {
+  if (reiki1.style.display === "none") {
+    reiki1.style.display = "block";
+  } else {
+    reiki1.style.display = "none";
+  }
+}
+
+let reflex = document.getElementById("Reflexology");
+function getElement5() {
+  if (reflex.style.display === "none") {
+    reflex.style.display = "block";
+  } else {
+    reflex.style.display = "none";
+  }
+}
+
+let myo = document.getElementById("myofascial");
+function getElement7() {
+  if (myo.style.display === "none") {
+    myo.style.display = "block";
+  } else {
+    myo.style.display = "none";
+  }
+}
+
+let preg = document.getElementById("pregnant");
+function getElement8() {
+  if (preg.style.display === "none") {
+    preg.style.display = "block";
+  } else {
+    preg.style.display = "none";
+  }
+}
