62815690a56e0376941314ec89b80c571fc513a9
Bit-Bytes-Bits <ian.kaibi@student.moringaschool.com>
2022-07-21 08:21:27 +0300

    added function to display two sections at a time

--- salon/assets/js/main.js
+++ salon/assets/js/main.js
@@ -6,13 +6,43 @@
 //     $(".content .radio_content").hide();
 //     $("."+current_raido).show();
 //   })
-var face = document.getElementById('main-content1');
+var face = document.getElementById("radio_content_radio_1");
 
 function getElement(){
     if(face.style.display === "none"){
-        face.style.display = "flex"
+        face.style.display = "block"
     }
     else{
         face.style.display = "none"
+    }
+}
+
+var nail = document.getElementById("radio_content_radio_2");
+function getElements(){
+    if(nail.style.display === "none"){
+        nail.style.display = "block"
+    }
+    else{
+        nail.style.display = "none"
+    }
+}
+
+var locks = document.getElementById("radio_content_radio_3");
+function getElements(){
+    if(locks.style.display === "none"){
+        locks.style.display = "block"
+    }
+    else{
+        locks.style.display = "none"
+    }
+}
+
+var braid = document.getElementById("radio_content_radio_4");
+function getElements(){
+    if(braid.style.display === "none"){
+        braid.style.display = "block"
+    }
+    else{
+        braid.style.display = "none"
     }
 }
