a61af7172e9b7d8ba466fce5e62bf8b11bbb503d
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-07-21 08:51:36 +0300

    add javascript and changes to barber html page

---
+++ barber/barber_assets/barber_script/script.js
@@ -0,0 +1,10 @@
+let man = document.getElementById('masculine');
+
+function getElement(){
+    if(man.style.display === "none"){
+        man.style.display ="block";
+    }
+    else{
+        man.style.display = "none";
+    }
+}
