961796790fe5aacb5d13deb27596c74f37371eb1
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-07-22 06:07:28 +0300

    connected barber main page and form page

--- barber/barber_assets/barber_script/script.js
+++ barber/barber_assets/barber_script/script.js
@@ -1,19 +1,22 @@
-let man = document.getElementById('masculine');
-let fem =document.getElementById('sassy')
-function getElement(){
-    if(man.style.display === "none"){
-        man.style.display ="block";
-    }
-    else{
-        man.style.display = "none";
-    }
+let man = document.getElementById("masculine");
+let fem = document.getElementById("sassy");
+function getElement() {
+  if (man.style.display === "none") {
+    man.style.display = "block";
+  } else {
+    man.style.display = "none";
+  }
 }
-function getElement2(){
-    console.log('working')
-    if(fem.style.display === "none"){
-        fem.style.display = "block";
-    }
-    else{
-        fem.style.display = "none";
-    }
+function getElement2() {
+  console.log("working");
+  if (fem.style.display === "none") {
+    fem.style.display = "block";
+  } else {
+    fem.style.display = "none";
+  }
+}
+
+let buttonClick = document.getElementsByClassName('card_order')
+function toForm(){
+    window.open("/form/form.html") 
 }
